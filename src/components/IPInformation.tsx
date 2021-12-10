faeec00010243f85d9fa1d09001e0584f87c2f87
avatarfreak <avatarfreak83@gmail.com>
2021-12-10 13:00:59 +0530

    styles and dynamic updated added

--- src/components/IPInformation.tsx
+++ src/components/IPInformation.tsx
@@ -1,41 +1,60 @@
-export const IPInformation = () => {
+import { IPProps } from "../../pages/[ip]";
+
+export const IPInformation: React.FC<Partial<IPProps>> = ({
+  ip,
+  region,
+  city,
+  country_code,
+  timezone_gmt,
+  isp,
+  success,
+}) => {
   return (
-    <div className="flex flex-col items-center py-6 rounded-xl shadow-lg bg-white absolute top-44 left-0 right-0 mx-6  z-[9999] ">
-      <div className="space-y-2 mb-5">
-        <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
-          Ip Address
-        </h2>
-        <p className="text-fs-400 text-clr-100 tracking-600 font-medium">
-          192.212.174.101
-        </p>
-      </div>
+    <>
+      {success && (
+        <div
+          className="text-center py-6 rounded-xl shadow-lg bg-white absolute top-44 left-0 right-0 mx-6  z-[9999]
+        md:grid md:grid-cols-4 md:top-56   md:px-6
+        md:divide-x-2 md:divide-gray-200 xl:max-w-[69.25rem] xl:m-auto
+        "
+        >
+          <div className="space-y-2 mb-5">
+            <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
+              Ip Address
+            </h2>
+            <p className="text-fs-400  text-clr-100 tracking-600 font-medium">
+              {ip}
+            </p>
+          </div>
 
-      <div className="space-y-2 mb-5">
-        <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
-          Location
-        </h2>
-        <p className="text-fs-300 text-clr-100 tracking-t-400 font-medium">
-          Brooklyn, NY 10001
-        </p>
-      </div>
+          <div className="space-y-2 mb-5">
+            <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
+              Location
+            </h2>
+            <p className="text-fs-300 text-clr-100 tracking-t-400 font-medium">
+              {`${region}, ${city} ${country_code}`}
+            </p>
+          </div>
 
-      <div className="space-y-2 mb-5">
-        <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
-          TimeZone
-        </h2>
-        <p className="text-fs-200 text-clr-100 tracking-t-400 font-medium">
-          utc - 05:00
-        </p>
-      </div>
+          <div className="space-y-2 mb-5">
+            <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
+              TimeZone
+            </h2>
+            <p className="text-fs-200 text-clr-100 tracking-t-400 font-medium">
+              {timezone_gmt}
+            </p>
+          </div>
 
-      <div className="space-y-2 mb-5">
-        <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
-          isp
-        </h2>
-        <p className="text-fs-100 text-clr-100 tracking-t-400 font-medium">
-          SpaceX Starlink
-        </p>
-      </div>
-    </div>
+          <div className="space-y-2 mb-5">
+            <h2 className="text-fs-500 text-clr-110 tracking-t-500 font-bold uppercase">
+              isp
+            </h2>
+            <p className="text-fs-100 text-clr-100 tracking-t-400 font-medium">
+              {isp}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
