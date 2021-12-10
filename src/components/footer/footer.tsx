44563cbd64d450207692a573f605cc20e322aa7a
avatarfreak <avatarfreak83@gmail.com>
2021-12-10 12:55:56 +0530

    Footer added

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="text-center text-xs tracking-wide">
+      Challenge by{" "}
+      <a
+        className="font-bold"
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://www.github.com/avatarfreak" className="font-bold">
+        avatarfreak
+      </a>
+      .
+    </footer>
+  );
+};
+export { Footer };
